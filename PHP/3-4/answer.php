abd5bb03c348189ef7a95352a6758550bb063dcc
nkssnkss <94731389+nkssnkss@users.noreply.github.com>
2022-01-25 20:42:44 +0900

    課題提出

--- PHP/3-4/answer.php
+++ PHP/3-4/answer.php
@@ -2,28 +2,36 @@
 
 <?php 
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
-$my_name = $_POST['my_name']
+$my_name = $_POST['my_name'];
+$q0 = $_POST['q0'];
 $q1 = $_POST['q1'];
 $q2 = $_POST['q2'];
-$q3 = $_POST['q3'];
-//選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
 ?>
+
+<php?
+function Judgement ($Q, $A) {
+    if ($Q == $A){
+            echo "正解";
+        } else {
+            echo "残念";
+        } 
+    } 
+?>
+
 <p><?php echo $my_name; ?>さんの結果は・・・？</p>
 
 <!--作成した関数を呼び出して結果を表示-->
 <p>①の答え
-    <br>
-    <?php if ($q1 = 80){
-            echo "正解";
-        } else {
-            echo "残念";
-        } ?>
+<br>
+<php?
+    Judgement($q0,80)
+?>
 </p>
 <br>
 
 <p>②の答え
     <br>
-    <?php if ($q2 = HTML){
+    <?php if ($q1 == "HTML"){
             echo "正解";
         } else {
             echo "残念";
@@ -31,7 +39,7 @@ $q3 = $_POST['q3'];
 
 <p>③の答え
     <br>
-    <?php if ($q3 = select){
+    <?php if ($q2 == "select"){
             echo "正解";
         } else {
             echo "残念";
--- PHP/3-4/question.php
+++ PHP/3-4/question.php
@@ -1,4 +1,7 @@
 <link rel="stylesheet" href="css/style.css">
+<?php $choices1 = ["80", "22", "20", "21"]; ?>
+<?php $choices2 = ["PHP", "Python", "JAVA", "HTML"]; ?>
+<?php $choices3 = ["join", "select", "insert", "update"]; ?>
 
 <?php
 //POST送信で送られてきた名前を受け取って変数を作成
@@ -9,29 +12,33 @@ $my_name = $_POST['my_name'];
 <form action="answer.php" method="post">
 
 <h2>①ネットワークのポート番号は何番？</h2>
-<input type="radio" name="q1" value="<?php echo $choice?> " 80/><?php echo $choice?>80
-<input type="radio" name="q1" value="<?php echo $choice?> " 22/><?php echo $choice?>>22
-<input type="radio" name="q1" value="<?php echo $choice?> " 20/><?php echo $choice?>>20
-<input type="radio" name="q1" value="<?php echo $choice?> " 21/><?php echo $choice?>>21
+<div>
+<?php foreach($choices1 as $choice): ?>
+<input type="radio" name="q0" value="<?php echo $choice?>" /><?php echo $choice?>
+<?php endforeach ?>
+</div>
 
 <h2>②Webページを作成するための言語は？</h2>
-<input type="radio" name="q2" value="PHP">PHP
-<input type="radio" name="q2" value="Python">Python
-<input type="radio" name="q2" value="JAVA">JAVA
-<input type="radio" name="q2" value="HTML">HTML
+<div>
+<?php foreach($choices2 as $choice): ?>
+<input type="radio" name="q1" value="<?php echo $choice?>" /><?php echo $choice?>
+<?php endforeach ?>
+</div>
 
 <h2>③MySQLで情報を取得するためのコマンドは？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-<input type="radio" name="q3" value="join">join
-<input type="radio" name="q3" value="select">select
-<input type="radio" name="q3" value="insert">insert
-<input type="radio" name="q3" value="update">update
+<div>
+<?php foreach($choices3 as $choice): ?>
+<input type="radio" name="q2" value="<?php echo $choice?>" /><?php echo $choice?>
+<?php endforeach ?>
+
 <br>
 <br>
 <br>
 
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+<input type="hidden" name="my_name" value="<?php echo $my_name; ?>" />
 <input type="submit" value="回答する" />
 </form>
-
+</div>
 
