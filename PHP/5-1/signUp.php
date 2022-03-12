358e5a0166ea64639fa204c03c131821fdb4ae1a
nkssnkss <94731389+nkssnkss@users.noreply.github.com>
2022-03-12 12:01:08 +0900

    ブログ作成

---
+++ PHP/5-1/signUp.php
@@ -0,0 +1,50 @@
+<?php
+require_once('db_connect.php');
+// セッション開始
+
+// ログインボタンが押された場合
+if (isset($_POST["signUp"])) {
+    if (!empty($_POST["name"]) && !empty($_POST["password"]) {
+        // 入力したユーザIDとパスワードを格納
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+// :passwordにバインドする場合は、$password_hashを使用する
+        $stmt->bindValue(':password', $password_hash);
+
+        $sql = "INSERT INTO users (name, password) VALUES ($name, $stmt)";
+
+        // 関数db_connect()からPDOを取得する
+        $pdo = db_connect();
+        try {
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            echo '登録が完了しました。';
+        } catch (PDOException $e) {
+            echo 'データベースエラー' . $e->getMessage();
+            die();
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<h1>新規登録</h1>
+<form method="POST" action="">
+user:<br>
+<input type="text" name="name" id="name">
+<br>
+password:<br>
+<input type="password" name="password" id="password"><br>
+<input type="submit" value="submit" id="signUp" name="signUp">
+
+</form>
+</body>
+</html>
